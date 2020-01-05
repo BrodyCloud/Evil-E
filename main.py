1226c8e32d53d774117f1922d0b3efe00ea1f505
Brody Cloud <andreabrodycloud@gmail.com>
2020-01-05 15:30:08 -0700

    Revert "Update how map handles Icons, Positions, and output."
    This reverts commit f7b121ca99ad6fb8a71c53fd9d9a4850ee083fa0.

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import random
 
-from math import ceil
-from evil_e.displays import display_generics, display_action
-from evil_e.utils import get_key
-from evil_e.entity import CreatePlayer, CreateFriendly, CreateEnemy
-from evil_e.map import CreateMap
-from evil_e.items import CreateHealthItem, CreateAttackItem
+from utils import get_key, clear
+from entity import CreatePlayer, CreateFriendly, CreateEnemy
+from themap import CreateMap
 
-# 90 x 42
-MAP_SIZE_X = 90
-MAP_SIZE_Y = 40
-AIR_SPACES = [15, 14, 12, 10, 8]
+MAP_SIZE = 40
+friend_count = 0
+enemy_count = 0
+world_items = 0
+characters = []
 friends = []
 enemies = []
 action = None
@@ -29,29 +27,30 @@ while True:
 	else:
 		break
 
-game_map = CreateMap(MAP_SIZE_X, MAP_SIZE_Y, AIR_SPACES[(difficulty - 1)])
-
-friend_count = (1 * difficulty)
+friend_count = (2 * difficulty)
 enemy_count = (5 * difficulty)
-world_items = round((.51 * difficulty))
-enemy_health = (50 + (15 * difficulty))
+world_items = (1 * difficulty)
+enemy_health = (50 * difficulty)
+
+game_map = CreateMap(MAP_SIZE, MAP_SIZE)
 
 # Build main character
 main = CreatePlayer(100, player_name)
-main.add_items(item_type='attack', requested_item="hands")
-main.add_items(item_type='attack', requested_item="one_shot")
-main.add_items(item_type='attack', requested_item="one_shot")
-main.add_items(item_type='health', requested_item="health_large")
+main.add_items(random_item=False, requested_item="hands")
+main.add_items(random_item=False, requested_item="one_shot")
+main.add_items(random_item=True)
+main.add_items(random_item=True)
 main.selected_item = main.inventory[0]
+
 main.location = random.choice(game_map.empty_spaces())
-game_map.update(main.location, main)
+game_map.map_update(main)
 
 # Build friends
 for count in range(friend_count):
 	working_name = "Friend #" + str(count)
 	working_identity = CreateFriendly(100, working_name)
 	working_identity.location = random.choice(game_map.empty_spaces())
-	game_map.update(working_identity.location, entity=working_identity)
+	game_map.map_update(working_identity)
 	working_identity.add_items(random_item=True)
 	working_identity.selected_item = working_identity.inventory[0]
 	friends.append(working_identity)
@@ -61,113 +60,73 @@ for count in range(enemy_count):
 	working_name = "Enemy #" + str(count)
 	working_identity = CreateEnemy(enemy_health, working_name)
 	working_identity.location = random.choice(game_map.empty_spaces())
-	game_map.update(working_identity.location, entity=working_identity)
-	# working_identity.add_items(random_item=True)
-	working_identity.add_items(requested_item="bow", item_type='attack')
+	game_map.map_update(working_identity)
+	working_identity.add_items(random_item=True)
 	working_identity.selected_item = working_identity.inventory[0]
 	enemies.append(working_identity)
 
-# Build random items in world
-for count in range(world_items):
-	working_item = random.choice((CreateAttackItem(), CreateHealthItem()))
-	working_item.random()
-	working_item.location = random.choice(game_map.empty_spaces())
-	game_map.update(working_item.location, entity=working_item)
-
 
 def game_loop():
-	main_action = False
-	enemy_action = False
+	display_for_round = False
 	while True:
-		display_generics(main, game_map)
-		if main_action:
-			display_action(main_action, game_map)
-			main_action = False
-		if enemy_action:
-			display_action(enemy_action, game_map)
-			enemy_action = False
+		clear()
+		game_map.print_to_screen()
+		print(f"""
+			\r Player Name: {main.name} | Coordinates: {main.location['x'], main.location['y']} | Health: {main.health} | Selected Item: {main.selected_item.name}
+			\r Inventory (+ and - to scroll): {list([item.name for item in main.inventory])} \n
+			\r Controls: W = Up, S = Down, A = Left, D = Right, E = Attack, L = Leave Game
+			""")
+		if display_for_round:
+			print(display_for_round)
+			display_for_round = False
 		while True:
 			action = get_key().decode()
-			if action == ("b" or "B"):
+			if action == ("l" or "L"):
 				exit()
-			elif action == ('p' or "P"):
+			elif action == ("w" or "w"):
+				main.move(game_map, {'x': 0, 'y': -1})
 				break
-			elif action == ("w" or "W"):
-				if main.move(game_map, {'x': 0, 'y': -1}):
-					print(main.location)
-					break
 			elif action == ("a" or "A"):
-				if main.move(game_map, {'x': -1, 'y': 0}):
-					break
+				main.move(game_map, {'x': -1, 'y': 0})
+				break
 			elif action == ("s" or "S"):
-				if main.move(game_map, {'x': 0, 'y': 1}):
-					break
+				main.move(game_map, {'x': 0, 'y': 1})
+				break
 			elif action == ("d" or "D"):
-				if main.move(game_map, {'x': 1, 'y': 0}):
-					break
+				main.move(game_map, {'x': 1, 'y': 0})
+				break
 			elif action == ("e" or "E"):
-				if isinstance(main.selected_item, CreateHealthItem):
-					main_action = "Healed: " + str(main.selected_item.heal)
-					main.modify_health(main.selected_item.heal, game_map)
+				# This is bad, remake items so that health and damage items are subclasses
+				if main.selected_item.consumable:
+					display_for_round = "Healed: " + str(main.selected_item.heal)
+					main.modify_health(main.selected_item.heal)
 					item_to_remove = main.selected_item
 					main.change_selected_item(-1)
 					main.remove_item(item_to_remove)
 					break
-					display_generics(main, game_map)
-			# TODO move entity kill to a single function
-			elif action == ("i" or "I"):
-				main_action = main.attack(game_map, {'x': 0, 'y': -1})
-				if 'kill' in main_action:
-					if isinstance(main_action['target_entity'], CreateEnemy):
-						print(enemies)
-						enemies.remove(main_action['target_entity'])
-					else:
-						friends.remove(main_action['target_entity'])
+				print("What direction to attack in? (W = Up, S = Down, A = Left, D = Right)")
+				action = get_key().decode()
+				if action == ("w" or "w"):
+					display_for_round = main.attack(game_map, {'x': 0, 'y': -1})
+					break
+				elif action == ("a" or "A"):
+					display_for_round = main.attack(game_map, {'x': -1, 'y': 0})
+					break
+				elif action == ("s" or "S"):
+					display_for_round = main.attack(game_map, {'x': 0, 'y': 1})
+					break
+				elif action == ("d" or "D"):
+					display_for_round = main.attack(game_map, {'x': 1, 'y': 0})
+					break
 				break
-			elif action == ("j" or "J"):
-				main_action = main.attack(game_map, {'x': -1, 'y': 0})
-				if 'kill' in main_action:
-					if isinstance(main_action['target_entity'], CreateEnemy):
-						enemies.remove(main_action['target_entity'])
-					else:
-						friends.remove(main_action['target_entity'])
-				break
-			elif action == ("k" or "K"):
-				main_action = main.attack(game_map, {'x': 0, 'y': 1})
-				if 'kill' in main_action:
-					if isinstance(main_action['target_entity'], CreateEnemy):
-						enemies.remove(main_action['target_entity'])
-					else:
-						friends.remove(main_action['target_entity'])
-				break
-			elif action == ("l" or "L"):
-				main_action = main.attack(game_map, {'x': 1, 'y': 0})
-				if 'kill' in main_action:
-					if isinstance(main_action['target_entity'], CreateEnemy):
-						enemies.remove(main_action['target_entity'])
-					else:
-						friends.remove(main_action['target_entity'])
-				break
-			# Changes selected item, doesn't count as a turn.
 			elif action == "+":
 				main.change_selected_item(1)
-				display_generics(main, game_map)
+				break
 			elif action == "-":
 				main.change_selected_item(-1)
-				display_generics(main, game_map)
+				break
 			else:
 				print("Invalid Action")
-		# Loop for all enemies and friends, to roam, enemies attack if player is targeted.
-		for enemy in enemies:
-			# Bit of an optimization. Enemy will only search for player if player is within 4 coordinates from them.
-			if (abs(main.location['x'] - enemy.location['x']) <= 4) and \
-					(abs(main.location['y'] - enemy.location['y']) <= 4):
-				enemy_action = enemy.search_area(game_map)
-			else:
-				enemy.do_roam(game_map, 3)
-
-		for friend in friends:
-			friend.do_roam(game_map, 10)
 
 
 game_loop()
